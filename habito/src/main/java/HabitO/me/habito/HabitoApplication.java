5f2d66cca0fe82f784d008f51ad4dd0e434f0a79
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2025-03-28 17:44:54 +0530

    messed

---
+++ habito/src/main/java/HabitO/me/habito/HabitoApplication.java
@@ -0,0 +1,13 @@
+package HabitO.me.habito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitoApplication.class, args);
+	}
+
+}
---
+++ habito/src/test/java/HabitO/me/habito/HabitoApplicationTests.java
@@ -0,0 +1,13 @@
+package HabitO.me.habito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
