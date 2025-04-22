675b8640a973d66bbe181ce43b0cc7b1eb8a7a81
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2025-04-23 00:14:02 +0530

    no-bug-js

--- habito/src/main/resources/static/app.js
+++ habito/src/main/resources/static/app.js
@@ -41,96 +41,108 @@ const grid = document.getElementById("streakGrid");
   document.addEventListener("DOMContentLoaded", () => {
     const boxes = document.querySelectorAll(".streak-box");
     boxes.forEach(box => {
-      const randomDelay = (Math.random() * 2).toFixed(2); 
-      box.style.animationDelay = `${randomDelay}s`;
+        const randomDelay = (Math.random() * 2).toFixed(2);
+        box.style.animationDelay = `${randomDelay}s`;
     });
-  });
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const buttonLogin = document.getElementById("login-btn");
-    const formBox = document.getElementById("form");
-    const buttonText = document.getElementById("buttonText");
-    function switchToLogin() {
-        formBox.innerHTML = `
-            <div class="input-field">
-                <label for="name">Username</label><br>
-                <div class="input-icon">
-                    <i class="fa-solid fa-user"></i>
-                    <input type="text" id="name" name="username" required>
-                </div>
-            </div>
-            
-            <div class="input-field">
-                <label for="password">Password</label><br>
-                <div class="input-icon">
-                    <i class="fa-solid fa-lock"></i>
-                    <input type="password" id="password" name="password" required>
-                </div>
-            </div>
-            
-            <div class="btn-n">
-                <button type="submit">Log In</button>
-                <p>Don't have an account? <a href="#" id="switchToRegister">Register</a></p>
-            </div>
-        `;
-        document.getElementById("switchToRegister").addEventListener("click", function (e) {
-            e.preventDefault();
-            buttonText.innerText = "Register";
-            switchToRegister();
-        });
-    }
-    function switchToRegister() {
-        formBox.innerHTML = `
-            <div class="input-field">
-                <label for="name">Username</label><br>
-                <div class="input-icon">
-                    <i class="fa-solid fa-user"></i>
-                    <input type="text" id="name" name="username" required>
-                </div>
-            </div>
-            
-            <div class="input-field">
-                <label for="email">Email</label><br>
-                <div class="input-icon">
-                    <i class="fa-solid fa-envelope"></i>
-                    <input type="email" id="email" name="email" required>
-                </div>
-            </div>
-            
-            <div class="input-field">
-                <label for="password">Password</label><br>
-                <div class="input-icon">
-                    <i class="fa-solid fa-lock"></i>
-                    <input type="password" id="password" name="password" required>
-                </div>
-            </div>
-            
-            <div class="input-field">
-                <label for="confirmPassword">Confirm Password</label><br>
-                <div class="input-icon">
-                    <i class="fa-solid fa-lock"></i>
-                    <input type="password" id="confirmPassword" name="confirmPassword" required>
-                </div>
-            </div>
-            <div class="btn-n">
-                <button type="submit">Register</button>
-                <p>Already have an account? <a href="#" id="switchToLogin">Login</a></p>
-            </div>
-        `;
-        document.getElementById("switchToLogin").addEventListener("click", function (e) {
-            e.preventDefault();
-            buttonText.innerText = "Login";
-            switchToLogin();
-        });
-    }
+});
 
-    buttonLogin.addEventListener("click", function () {
-        if (buttonLogin.innerText === "Login") {
-            buttonText.innerText = "Register";
-            switchToLogin();
-        } else {
-            buttonText.innerText = "Login";
-            switchToRegister();
-        }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonLogin = document.getElementById("login-btn");
+  const formBox = document.getElementById("form");
+  const buttonText = document.getElementById("buttonText");
+
+  function switchToLogin() {
+      formBox.innerHTML = `
+          <div class="input-field">
+              <label for="name">Username</label><br>
+              <div class="input-icon">
+                  <i class="fa-solid fa-user"></i>
+                  <input type="text" id="name" name="username" required>
+              </div>
+          </div>
+          
+          <div class="input-field">
+              <label for="password">Password</label><br>
+              <div class="input-icon">
+                  <i class="fa-solid fa-lock"></i>
+                  <input type="password" id="password" name="password" required>
+              </div>
+          </div>
+          
+          <div class="btn-n">
+              <button type="submit">Log In</button>
+              <p>Don't have an account? <a href="#" id="switchToRegister">Register</a></p>
+          </div>
+      `;
+
+      const switchToRegisterLink = document.getElementById("switchToRegister");
+      switchToRegisterLink.addEventListener("click", function (e) {
+          e.preventDefault(); 
+          buttonText.innerText = "Login";
+          switchToRegister();
+      });
+  }
+
+  function switchToRegister() {
+      formBox.innerHTML = `
+          <div class="input-field">
+              <label for="name">Username</label><br>
+              <div class="input-icon">
+                  <i class="fa-solid fa-user"></i>
+                  <input type="text" id="name" name="username" required>
+              </div>
+          </div>
+          
+          <div class="input-field">
+              <label for="email">Email</label><br>
+              <div class="input-icon">
+                  <i class="fa-solid fa-envelope"></i>
+                  <input type="email" id="email" name="email" required>
+              </div>
+          </div>
+          
+          <div class="input-field">
+              <label for="password">Password</label><br>
+              <div class="input-icon">
+                  <i class="fa-solid fa-lock"></i>
+                  <input type="password" id="password" name="password" required>
+              </div>
+          </div>
+          
+          <div class="input-field">
+              <label for="confirmPassword">Confirm Password</label><br>
+              <div class="input-icon">
+                  <i class="fa-solid fa-lock"></i>
+                  <input type="password" id="confirmPassword" name="confirmPassword" required>
+              </div>
+          </div>
+          <div class="btn-n">
+              <button type="submit">Register</button>
+              <p>Already have an account? <a href="#" id="switchToLogin">Login</a></p>
+          </div>
+      `;
+      const switchToLoginLink = document.getElementById("switchToLogin");
+      switchToLoginLink.addEventListener("click", function (e) {
+          e.preventDefault();
+          buttonText.innerText = "Register";
+          switchToLogin();
+      });
+  }
+  const initialSwitchToLoginLink = document.getElementById("switchToLogin");
+  if (initialSwitchToLoginLink) {
+      initialSwitchToLoginLink.addEventListener("click", function (e) {
+          e.preventDefault();
+          buttonText.innerText = "Register";
+          switchToLogin();
+      });
+  }
+  buttonLogin.addEventListener("click", function () {
+      if (buttonText.innerText === "Login") {
+          buttonText.innerText = "Register";
+          switchToLogin();
+      } else {
+          buttonText.innerText = "Login";
+          switchToRegister();
+      }
+  });
 });
