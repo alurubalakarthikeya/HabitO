c11dc41b116fa0597efc9b2221dc2d70af9eb833
Bala Karthikeya Aluru <alurubalakarthikeya@gmail.com>
2025-05-04 11:31:43 +0530

    trauma

--- habito/src/main/java/HabitO/me/habito/HabitoApplication.java
+++ habito/src/main/java/HabitO/me/habito/HabitoApplication.java
@@ -1,13 +1,9 @@
 package HabitO.me.habito;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class HabitoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HabitoApplication.class, args);
 	}
-
 }
